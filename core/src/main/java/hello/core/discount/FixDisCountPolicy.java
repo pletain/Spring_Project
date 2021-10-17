befefca80f627af15682848d60287baae2767c5b
pletain <pletain@dgu.ac.kr>
2021-10-17 20:19:57 +0900

    주문 & 할인 도메인 개발 중

---
+++ core/src/main/java/hello/core/discount/FixDisCountPolicy.java
@@ -0,0 +1,14 @@
+package hello.core.discount;
+
+import hello.core.member.Member;
+
+public class FixDisCountPolicy implements DiscountPolicy {
+
+    @Override
+    public int discount(Member member, int price) {
+        if()
+        return 0;
+    }
+    
+    
+}
