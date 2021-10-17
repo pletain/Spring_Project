cfb3ead49b78b4955e038e02eca10a12e0e07b16
pletain <pletain@dgu.ac.kr>
2021-10-17 17:57:52 +0900

    회원 도메인 개발

--- core/src/main/java/hello/core/member/Member.java
+++ core/src/main/java/hello/core/member/Member.java
@@ -5,11 +5,29 @@ public class Member {
     private String name;
     private Grade grade;
 
-    public Member(Long id, String name, Grade grade) {
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
         this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
         this.name = name;
+    }
+
+    public Grade getGrade() {
+        return this.grade;
+    }
+
+    public void setGrade(Grade grade) {
         this.grade = grade;
     }
 
-   
+    
 }
