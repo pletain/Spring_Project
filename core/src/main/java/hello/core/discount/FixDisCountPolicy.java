03864cf16945d41f41af6ffa1b2c7c51e4704e12
pletain <pletain@dgu.ac.kr>
2021-10-23 20:25:29 +0900

    Appconfig setting

--- core/src/main/java/hello/core/discount/FixDisCountPolicy.java
+++ core/src/main/java/hello/core/discount/FixDisCountPolicy.java
@@ -8,6 +8,7 @@ public class FixDisCountPolicy implements DiscountPolicy {
 
     private int discountFixAmount = 1000;
 
+    
     @Override
     public int discount(Member member, int price) {
         if(member.getGrade() == Grade.VIP)
