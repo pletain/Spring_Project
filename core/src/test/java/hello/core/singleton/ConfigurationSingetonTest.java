f2e7761f102c2e31d6cb6ab71d54c753b3657471
pletain@dgu.ac.kr <pletain@dgu.ac.kr>
2021-11-01 20:21:26 +0900

    @Configuration과 싱글톤

---
+++ core/src/test/java/hello/core/singleton/ConfigurationSingetonTest.java
@@ -0,0 +1,31 @@
+package hello.core.singleton;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import hello.core.AppConfig;
+import hello.core.Order.OrderServiceImpl;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemberServiceImpl;
+
+public class ConfigurationSingetonTest {
+    ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    @Test
+    void configurationTest() {
+        MemberServiceImpl memberService = ac.getBean("memberService", MemberServiceImpl.class);
+        OrderServiceImpl orderService = ac.getBean("orderService", OrderServiceImpl.class);
+        MemberRepository memberRepository = ac.getBean("memberRepository", MemberRepository.class);
+    
+        System.out.println("memberService -> MemberRepository = " + memberService.getMemberRepository());
+        System.out.println("orderService -> MemberRepository = " + orderService.getMemberRepository());
+        System.out.println("memberRepositoyr -> MemberRepository = " + memberRepository);
+
+        Assertions.assertThat(memberService.getMemberRepository()).isSameAs(memberRepository);
+        Assertions.assertThat(orderService.getMemberRepository()).isSameAs(memberRepository);
+    }
+    
+}
+
